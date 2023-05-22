44089d3e2f792b05af50976a557da9fad4a3db9d
MohannadNawasreh <mohanned4x@gmail.com>
2023-05-22 14:03:02 +0300

    test

--- lib/layout/profile.dart
+++ lib/layout/profile.dart
@@ -16,35 +16,43 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage("images/Untitled-2.png"),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              
+              children: [
+                
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage("images/Untitled-2.png"),
+                  
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("${passenger.userName}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 3, 184, 78)),),
+                    SizedBox(width: 10,),
+                    Text("4.5", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber[300]),),
+                    Icon(Icons.star_half_rounded, color: Colors.amber[300],),
+                  ],
+                ),
+                
+                
+                SizedBox(height: 10,),
+                Text(
+                  "${passenger.email}"),
+                SizedBox(height: 10,),
+                Text("${passenger.phoneNumber}"),
+                SizedBox(height: 10,),
+                
+              ],
               
             ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("${passenger.userName}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 3, 184, 78)),),
-                SizedBox(width: 10,),
-                Text("4.5", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber[300]),),
-                Icon(Icons.star_half_rounded, color: Colors.amber[300],),
-              ],
-            ),
-
-            
-            SizedBox(height: 20,),
-            Text("${passenger.email}"),
-            SizedBox(height: 20,),
-            Text("${passenger.phoneNumber}"),
-            SizedBox(height: 20,),
-            
-          ],
-          
+          ),
         ),
       ),
     );
